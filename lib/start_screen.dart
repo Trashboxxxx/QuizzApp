cda0f6d48fcf8d6bf6d035fb802bed6e1f2e61cb
Trashboxxxx <wasuthornkrutsuwan@gmail.com>
2025-02-07 10:15:20 +0700

    update

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'questions_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
@@ -22,6 +23,15 @@ class StartScreen extends StatelessWidget {
                 child: Image.asset(
                   'assets/images/quiz-logo.png',
                   width: 300,
+                ),
+              ),
+              const SizedBox(height: 30),
+              Text(
+                'Learn Flutter the fun way!',
+                style: GoogleFonts.lato(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
               const SizedBox(height: 30),
